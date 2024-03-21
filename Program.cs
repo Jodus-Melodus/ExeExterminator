35c81fe3bf6e8debeddc7f705bbdb3a2b4173a18
Jodus-Melodus <philipgricius@gmail.com>
2024-03-21 11:33:45 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+class Program
+{
+
+    private static int TotalDeletedFiles {get; set;} = 0;
+
+    public static string[] GetDirectories(string path)
+    {
+        return Directory.GetDirectories(path);
+    }
+
+    public static string[] GetFiles(string path)
+    {
+        return Directory.GetFiles(path);
+    }
+
+    public static void DeleteFiles(string path, string fileExtention)
+    {
+        Directory.SetCurrentDirectory(path);
+        string[] dirs = GetDirectories(path);
+        string[] files = GetFiles(path);
+
+        foreach (string subPath in dirs)
+        {
+            DeleteFiles(subPath, fileExtention);
+        }
+
+        foreach (string subPath in files)
+        {
+            if (subPath.Contains(fileExtention))
+            {
+                File.Delete(subPath);
+                Console.WriteLine("Deleted : " + Path.GetFileName(subPath));
+                TotalDeletedFiles += 1;
+            }
+        }
+    }
+
+    public static string? ReadLine(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
+
+    public static void Main()
+    {
+        string? path = ReadLine("Enter path > ");
+        string? fileExtentionToDelete = ReadLine("Enter extention to exterminate > ");
+
+        if ((fileExtentionToDelete != null) && (path != null))
+        {
+            DeleteFiles(path, fileExtentionToDelete);
+        }
+
+        Console.WriteLine("Deleted " + TotalDeletedFiles + "files.");
+    }
+}
